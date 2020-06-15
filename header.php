2b6dfce05513ac1a7ee02760eb6a33a8d26fd4e0
r2on11 <61480893+r2on11@users.noreply.github.com>
2020-06-15 16:08:35 +0200

    responsive header

--- header.php
+++ header.php
@@ -8,22 +8,32 @@
 
     <body>
 
-       <!-- <?php wp_nav_menu(array('theme_location'=>'primarymenu')); ?> -->
-       
-            <nav class="navbar navbar-expand-lg bg-light ">
-                <a class="navbar-brand" href="http://localhost/testsite/wordpress/">EkkoCharity</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-                </button>
+    <html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ekko Charity</title>
+    <?php wp_head(); ?>
+</head>
+<body>
+    <!-- <?php wp_nav_menu(array('theme_location'=>'primarymenu')); ?> -->
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <?php wp_nav_menu( array (
-                    'theme_location' => 'primarymenu',
-                    'container' => false,
-                    'menu_class' => 'navbar-nav m-auto ' 
-                    ) ); ?>  
-                </div>
+    <nav class="navbar navbar-expand-lg = navbar-dark bg-light p-0">
+        <a class="navbar-brand p-lg-5" href="http://localhost/testsite/wordpress/">EkkoCharity</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+         <span class="navbar-toggler-icon"></span>
+        </button>
 
-                <button id="butoniNav" type="button" class="btn btn-primary">Get Involved </button>
+         <div class="collapse navbar-collapse navbar-light bg-light" id="navbarSupportedContent">
+            <?php wp_nav_menu( array (
+            'theme_location' => 'primarymenu',
+            'container' => false,
+            'menu_class' => 'navbar-nav m-auto ' 
+            ) ); ?>  
+         </div>
+         </div>
+            <div class="butonitest d-none d-lg-inline">
+             <button id="butoniNav" type="button" class="btn btn-primary">Get Involved </button>
+         </div>
 
-            </nav>
+    </nav>
