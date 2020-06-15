7520dee9ef4309d6332297d7369ac465d29a8584
r2on11 <61480893+r2on11@users.noreply.github.com>
2020-06-15 14:26:43 +0200

    addnav+style
    Adding navbar+style at header and updating functions

--- header.php
+++ header.php
@@ -7,3 +7,23 @@
     </head>
 
     <body>
+
+       <!-- <?php wp_nav_menu(array('theme_location'=>'primarymenu')); ?> -->
+       
+            <nav class="navbar navbar-expand-lg bg-light ">
+                <a class="navbar-brand" href="http://localhost/testsite/wordpress/">EkkoCharity</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <?php wp_nav_menu( array (
+                    'theme_location' => 'primarymenu',
+                    'container' => false,
+                    'menu_class' => 'navbar-nav m-auto ' 
+                    ) ); ?>  
+                </div>
+
+                <button id="butoniNav" type="button" class="btn btn-primary">Get Involved </button>
+
+            </nav>
