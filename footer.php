95aa2ccd160c8f506f836579f805c54a00f1e360
Donika Tomiqi <donikatomiqi@gmail.com>
2020-06-15 17:07:20 +0200

    Style footer

--- footer.php
+++ footer.php
@@ -1,6 +1,73 @@
-    <footer>
-        This is my footer!
-    </footer>
+        <footer class="footer mt-auto py-3">
+            <div class="row">
+                <div class="d-flex col-sm-12 ">
+                    <div class="footer-navbar">
+                        <?php
+                    wp_nav_menu( array(
+                    'theme_location'    => 'secondary',
+                    'container'         => 'false',
+                    'menu_class'        => 'navbar-nav flex-md-column flex-lg-row'
+                    ) );
+                ?>
+                    </div>
+                    <div class="ml-auto px-3 text-right">
+                        <span class="mx-3"><i class="fa fa-google"></i></span>
+                        <span class="mx-3"><i class="fa fa-instagram"></i></span>
+                        <span class="mx-3"><i class="fa fa-twitter"></i></span>
+                    </div>
+                </div>
+            </div>
 
+            <div class="pt-4 mb-md-5 mt-4 pt-md-5 border-top border-bottom pb-4">
+                <div class="row footer-links">
+                    <div class="col-6 col-md footer-links">
+                        <h5>About</h5>
+                        <ul class="list-unstyled text-small">
+                            <li><a class="text-muted" href="#">Cool stuff</a></li>
+                            <li><a class="text-muted orange" href="#">Random feature</a></li>
+                            <li><a class="text-muted orange" href="#">Team feature</a></li>
+                            <li><a class="text-muted orange" href="#">Stuff for developers</a></li>
+                            <li><a class="text-muted orange" href="#">Another one</a></li>
+                            <li><a class="text-muted orange" href="#">Last time</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-6 col-md">
+                        <h5>Additional Links</h5>
+                        <ul class="list-unstyled text-small">
+                            <li><a class="text-muted" href="#">Cool stuff</a></li>
+                            <li><a class="text-muted" href="#">Random feature</a></li>
+                            <li><a class="text-muted" href="#">Team feature</a></li>
+                            <li><a class="text-muted" href="#">Stuff for developers</a></li>
+                            <li><a class="text-muted" href="#">Another one</a></li>
+                            <li><a class="text-muted" href="#">Last time</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-6 col-md">
+                        <h5>Categories</h5>
+                        <ul class="list-unstyled text-small">
+                            <li><a class="text-muted" href="#">Resource</a></li>
+                            <li><a class="text-muted" href="#">Resource name</a></li>
+                            <li><a class="text-muted" href="#">Another resource</a></li>
+                            <li><a class="text-muted" href="#">Final resource</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-6 col-md">
+                        <h5>Ekko</h5>
+                        <ul class="list-unstyled text-small">
+                            <li><a class="text-muted" href="#">Team</a></li>
+                            <li><a class="text-muted" href="#">Locations</a></li>
+                            <li><a class="text-muted" href="#">Privacy</a></li>
+                            <li><a class="text-muted" href="#">Terms</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <p class="text-sm-center text-muted">@ Phoenix Charity</p>
+
+        </footer>
+      <?php //wp_footer() ?>
+
+        </div>
     </body>
 </html>
--- header.php
+++ header.php
@@ -1,39 +1,35 @@
-<!doctype html>
-<html>
-    <head> 
-        <meta charset="utf-8">
-        <title>Phoenix Theme</title>
-        <?php wp_head(); ?>
-    </head>
+<!DOCTYPE html>
+<html lang="en">
 
-    <body>
-
-    <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ekko Charity</title>
+    <title>Phoenix Theme</title>
     <?php wp_head(); ?>
+    <script src="https://use.fontawesome.com/6cadc76375.js"></script>
 </head>
+
 <body>
-    <!-- <?php wp_nav_menu(array('theme_location'=>'primarymenu')); ?> -->
+    <div class="container">
+        <!-- <?php wp_nav_menu(array('theme_location'=>'primarymenu')); ?> -->
 
-    <nav class="navbar navbar-expand-lg = navbar-dark bg-light p-0">
-        <a class="navbar-brand p-lg-5" href="http://localhost/testsite/wordpress/">EkkoCharity</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-         <span class="navbar-toggler-icon"></span>
-        </button>
+        <nav class="navbar navbar-expand-lg = navbar-dark bg-light p-0">
+            <a class="navbar-brand p-lg-5" href="http://localhost/testsite/wordpress/">EkkoCharity</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-         <div class="collapse navbar-collapse navbar-light bg-light" id="navbarSupportedContent">
-            <?php wp_nav_menu( array (
+            <div class="collapse navbar-collapse navbar-light bg-light" id="navbarSupportedContent">
+                <?php wp_nav_menu( array (
             'theme_location' => 'primarymenu',
             'container' => false,
             'menu_class' => 'navbar-nav m-auto ' 
-            ) ); ?>  
-         </div>
-         </div>
-            <div class="butonitest d-none d-lg-inline">
-             <button id="butoniNav" type="button" class="btn btn-primary">Get Involved </button>
-         </div>
+            ) ); ?>
+            </div>
 
-    </nav>
+            <div class="butonitest d-none d-lg-inline">
+                <button id="butoniNav" type="button" class="btn btn-primary">Get Involved </button>
+            </div>
+
+        </nav>
