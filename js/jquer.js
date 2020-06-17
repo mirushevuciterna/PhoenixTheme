8965b3f2cbd3b24b137aae9e962f05f4ae6aaa98
r2on11 <61480893+r2on11@users.noreply.github.com>
2020-06-17 19:36:03 +0200

    asdasd

--- js/jquer.js
+++ js/jquer.js
@@ -30,34 +30,4 @@ $('.owl-carousel').owlCarousel({
     }
 });
 
-// $('.owl-carousel1').owlCarousel({
-//     item:3,
-//     singleItem: true,
-//     autoplay: true,
-//     autoplayTimeout: 3000,
-//     loop: true,
-//     nav: false,
-//     responsiveClass: true,
-//     responsive:{
-//         0:{
-//             items: 2,
-//             nav: false,
-//             dots: false
-//         },
-//         600:{
-//             items: 3,
-//             nav: false,
-//             dots: false
-//         },
-//         960:{
-//             items:5,
-//             nav: false,
-//             dots: false
-//         },
-//         1200:{
-//             items: 6,
-//             nav: false,
-//             dots: false
-//         }
-//     }
-// });
+
