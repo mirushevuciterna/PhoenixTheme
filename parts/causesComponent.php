206220f36ef57266a5b9b6771c8806e97210cbc5
r2on11 <61480893+r2on11@users.noreply.github.com>
2020-06-17 15:36:08 +0200

    Causes comp version 1.0

---
+++ parts/causesComponent.php
@@ -0,0 +1,108 @@
+
+
+
+
+<div class="causes">
+    <!-- <div class="container"> -->
+        <div class="container-xl d-flex justify-content-center align-items-center flex-column">
+            <h1 > Causes We Support </h1> 
+            <p ">We’re here for people like you who want to make a difference. Choose how and when 
+                you support the causes you care about.</p>
+        </div>
+
+
+        <!-- <div class="bgcolor"> -->
+			<div class="containercontent1">
+
+				<div class="owl-carousel1 owl-theme">
+                    <div class="row">
+                        <div class="col-md-4 d-flex justify-content-center align-items-center">
+                            <div class="card" style="width: 18rem;">
+                                <img class="card-img-top" src="..." alt="Card image cap">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <div class="butonitest d-none d-lg-inline">
+                                            <button id="butoniNav" type="button" class="btn btn-primary">Get Involved </button>
+                                        </div>
+                                    </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 d-flex justify-content-center align-items-center">
+                            <div class="card" style="width: 18rem;">
+                                <img class="card-img-top" src="..." alt="Card image cap">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <div class="butonitest d-none d-lg-inline">
+                                            <button id="butoniNav" type="button" class="btn btn-primary">Get Involved </button>
+                                        </div>
+                                    </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 d-flex justify-content-center align-items-center">
+                            <div class="card" style="width: 18rem;">
+                                <img class="card-img-top" src="..." alt="Card image cap">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <div class="butonitest d-none d-lg-inline">
+                                            <button id="butoniNav" type="button" class="btn btn-primary">Get Involved </button>
+                                        </div>
+                                    </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 d-flex justify-content-center align-items-center">
+                            <div class="card" style="width: 18rem;">
+                                <img class="card-img-top" src="..." alt="Card image cap">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <div class="butonitest d-none d-lg-inline">
+                                            <button id="butoniNav" type="button" class="btn btn-primary">Get Involved </button>
+                                        </div>
+                                    </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 d-flex justify-content-center align-items-center">
+                            <div class="card" style="width: 18rem;">
+                                <img class="card-img-top" src="..." alt="Card image cap">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <div class="butonitest d-none d-lg-inline">
+                                            <button id="butoniNav" type="button" class="btn btn-primary">Get Involved </button>
+                                        </div>
+                                    </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 d-flex justify-content-center align-items-center">
+                            <div class="card" style="width: 18rem;">
+                                <img class="card-img-top" src="..." alt="Card image cap">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <div class="butonitest d-none d-lg-inline">
+                                            <button id="butoniNav" type="button" class="btn btn-primary">Get Involved </button>
+                                        </div>
+                                    </div>
+                            </div>
+                        </div>
+                        <!-- <div class="col-md-4">
+                            <div class="card" style="width: 18rem;">
+                                <img class="card-img-top" src="..." alt="Card image cap">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <div class="butonitest d-none d-lg-inline">
+                                            <button id="butoniNav" type="button" class="btn btn-primary">Get Involved </button>
+                                        </div>
+                                    </div>
+                            </div>
+                        </div> -->
+                    </div>
+				</div>				
+			</div>
+		</div>
+    </div>            
+</div>
