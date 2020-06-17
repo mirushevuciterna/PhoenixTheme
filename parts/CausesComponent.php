b193094b4da4673c15d404bd4f620c77814a255c
Shuajb Qerimi <shuajbqerimi@gmail.com>
2020-06-17 19:28:00 +0200

    RRegullimi i carouselit

---
+++ parts/CausesComponent.php
@@ -0,0 +1,79 @@
+<div class="d-flex justify-content-center align-items-center flex-column" style="background-color: #fafafa;">
+            <h1 > Causes We Support </h1> 
+            <p ">We’re here for people like you who want to make a difference. Choose how and when 
+            you support the causes you care about.</p>
+        </div>
+        
+        
+        <div class="bgcolor1">
+			<div class="containercontent1">
+
+				<div class="owl-carousel owl-theme">
+					<div class="item foto1">
+                            <div class="card p-3 cardthyrje">
+                                <img class="card-img-top fotoinside" src="<?php echo get_stylesheet_directory_uri();?>/images/charity3.jpg" alt="Card image cap">
+                                <div class="card-body">
+                                    <h4 class="card-title">Animal welfare</h4>
+                                    <small>
+                                        <p>This is a longer card with supporting text below as a natural lead-in to additional content</p>
+                                    </small>
+                                    <div class="butonitest d-none d-lg-inline">
+                                        <button id="butoniNav" type="button" class="btn btn-primary">Get Involved </button>
+                                    </div>
+                                </div>
+                            </div>
+                    </div>
+					<div class="item foto1">
+                            <div class="card p-3 cardthyrje">
+                            <img class="card-img-top img-fluid fotoinside" src="<?php echo get_stylesheet_directory_uri();?>/images/charity4.jpg" alt="Card image cap">
+                            <div class="card-body">
+                                <h4 class="card-title">Child assistance</h4>
+                                <small>
+                                    <p>This is a longer card with supporting text below as a natural lead-in to additional content</p>
+                                </small>
+                                <div class="butonitest d-none d-lg-inline">
+                                    <button id="butoniNav" type="button" class="btn btn-primary">Get Involved </button>
+                                </div>
+                            </div>
+                            </div>
+                    </div>
+					<div class="item foto1">
+                            <div class="card p-3 cardthyrje">
+                            <img class="card-img-top img-fluid fotoinside" src="<?php echo get_stylesheet_directory_uri();?>/images/charity5.jpg" alt="Card image cap">
+                            <div class="card-body">
+                                <h4 class="card-title">Disaster Relief</h4>
+                                <small>
+                                    <p>This is a longer card with supporting text below as a natural lead-in to additional content</p>
+                                </small>
+                                <div class="butonitest d-none d-lg-inline">
+                                    <button id="butoniNav" type="button" class="btn btn-primary">Get Involved </button>
+                                </div>
+                            </div>
+                            </div>
+                    </div>
+					<div class="item foto1">
+                            <div class="card p-3 cardthyrje">
+                            <img class="card-img-top img-fluid fotoinside" src="<?php echo get_stylesheet_directory_uri();?>/images/charity2.jpg" alt="Card image cap">
+                            <div class="card-body">
+                                <h4 class="card-title">Youth development</h4>
+                                <small>
+                                    <p>This is a longer card with supporting text below as a natural lead-in to additional content</p>
+                                </small>
+                                <div class="butonitest d-none d-lg-inline">
+                                    <button id="butoniNav" type="button" class="btn btn-primary">Get Involved </button>
+                                </div>
+                            </div>
+                            </div>
+                    </div>
+
+					
+					
+					
+
+
+
+					
+				</div>				
+			</div>
+		</div>
+		
