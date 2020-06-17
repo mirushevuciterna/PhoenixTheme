79976e759fcb7f790ebb04cd2e0837f3e2bd8298
Donika Tomiqi <donikatomiqi@gmail.com>
2020-06-17 15:29:18 +0200

    added paragraph donate feature

---
+++ parts/donateComponent.php
@@ -0,0 +1,6 @@
+<div class="p-5">
+    <div class="pg_row text-center">
+        <h6><span>Learn More</span> about Ekko's unique features.</h6>
+        <h2>A better way to give</h2>
+    </div>
+</div>
