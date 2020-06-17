532d2a17617be7d3c927f02200e8a5ed8b3a327c
pashtriga <pashtrik.gashi1@gmail.com>
2020-06-17 16:13:00 +0200

    added the icons on the donate section, stylized it with css and made it responsive

--- parts/donateComponent.php
+++ parts/donateComponent.php
@@ -1,26 +1,37 @@
 <div class="p-5">
     <div class="pg_row text-center">
-        <h6><span>Learn More</span> about Ekko's unique features.</h6>
+        <h6>
+            <span>Learn More</span> about Ekko's unique features.</h6>
         <h2>A better way to give</h2>
     </div>
 
     <div class="row">
         <div class="col-md-4">
-            <i class="fa fa-paypal"></i>
-            <h4>Easy and simple payments</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem ipsum dolor sit amet, consectetur</p>
-            <i class="fa fa-gift"></i>
-            <h4>Effective giving options</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem ipsum dolor sit amet, consectetur</p>
+            <div class="donateInfo">
+                <i class="fa fa-paypal" id="ikonat"></i>
+                <h4>Easy and simple payments</h4>
+                <p class="donateText">Lorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem ipsum dolor sit amet, consectetur</p>
+            </div>
+
+            <div class="donateInfo">
+                <i class="fa fa-gift" id="ikonat"></i>
+                <h4>Effective giving options</h4>
+                <p class="donateText">Lorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem ipsum dolor sit amet, consectetur</p>
+            </div>
         </div>
 
         <div class="col-md-4">
-            <i class="fa fa-globe"></i>
-            <h4>Donate worldwide</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem ipsum dolor sit amet, consectetur</p>
-            <i class="fa fa-envelope"></i>
-            <h4>Charitable service</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem ipsum dolor sit amet, consectetur</p>
+            <div class="donateInfo">
+                <i class="fa fa-globe" id="ikonat"></i>
+                <h4>Donate worldwide</h4>
+                <p class="donateText">Lorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem ipsum dolor sit amet, consectetur</p>
+            </div>
+
+            <div class="donateInfo">
+                <i class="fa fa-envelope" id="ikonat"></i>
+                <h4>Charitable service</h4>
+                <p class="donateText">Lorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem ipsum dolor sit amet, consectetur</p>
+            </div>
         </div>
 
         <div class="col-md-4">
