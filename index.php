14fa3b404398b6f276d8195c87d1ee7c519608d1
pashtriga <pashtrik.gashi1@gmail.com>
2020-06-17 10:47:24 +0200

    minor changees

--- index.php
+++ index.php
@@ -1,19 +1,6 @@
 <?php get_header(); ?>
-<<<<<<< HEAD
-<div class="containeri">
-    <div class="container-fluid">
-        <div class="content">
-            <h1>We need your help. Get involved!</h1>
-            <p>Lorem ipsum dolor sit amet dictum sit amet justo donec enim diam vulputate.</br> Vitae suscipit tellus mauris a diam
-                maecenas sed enim ut habitasse platea dictumst.</p>
-            <button type="button" class="btn btn-primary btn-lg">Puchase Ekko</button>
-        </div>
-    </div>
-</div>
-=======
 <?php get_template_part('parts/TextOnImage');
       get_template_part('parts/logosComponent');
 ?>
 
->>>>>>> 7dabfd46e8467ead6eec5af2fa93640b766fe038
 <?php get_footer(); ?>
