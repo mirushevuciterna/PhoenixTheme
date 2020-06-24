402bc79be6b29ed90594a5634bed13201a96fd23
pashtriga <pashtrik.gashi1@gmail.com>
2020-06-24 16:39:31 +0200

    made a new css file for donationVideo.php and made it full responsive

--- parts/donationVideo.php
+++ parts/donationVideo.php
@@ -1,6 +1,7 @@
 <?php
     if( have_rows('video_youtube1') ):
         while ( have_rows('video_youtube1') ) : the_row(); ?>
+        <?php if (get_sub_field('position') == 'left_video') { ?>
             <div class="donationVideoFeature">
                 <div class="video-container">
                     <?php the_sub_field('video'); ?>
@@ -10,9 +11,21 @@
                     <p class="donation-paragraph"><?php the_sub_field('paragrafi_video');?></p>
                     <button class="btn btn-lg donateBTN"><?php the_sub_field('butoni_video');?></button>
                 </div>
-            </div>   
-        <?php endwhile;
-        else :
-        // no rows found
-    endif;
-?>
+            </div>
+        <?php } else { ?>
+            <div class="donationVideoFeature">
+                <div class="donationVideoText p-5 ">
+                    <h3 class="donation-title"><?php the_sub_field('titulli_video');?></h3>
+                    <p class="donation-paragraph"><?php the_sub_field('paragrafi_video');?></p>
+                    <button class="btn btn-lg donateBTN"><?php the_sub_field('butoni_video');?></button>
+                </div>
+                <div class="video-container">
+                    <?php the_sub_field('video'); ?>
+                </div>
+            </div>
+    <?php 
+        }
+        endwhile;
+                
+        endif;
+    ?>
