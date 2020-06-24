bae3b881ce5be002d3723102b92f0b1fa4c34ffd
r2on11 <61480893+r2on11@users.noreply.github.com>
2020-06-24 15:35:20 +0200

    sda

--- modules/volunteerscomponent.php
+++ modules/volunteerscomponent.php
@@ -9,15 +9,15 @@
 			<div class="containercontent1VOL">
 				<div class="owl-caroVOL owl-carousel owl-theme">
                     <?php
-                        if( have_rows('volunteers_settings') ):
-                            while ( have_rows('volunteers_settings') ) : the_row(); ?>
+                        if( have_rows('vol_set') ):
+                            while ( have_rows('vol_set') ) : the_row(); ?>
                             <div class="item foto1VOL">
                             <div class="card p-3 cardthyrjeVOL">
-                                <img class="card-img-top fotoinsideVOL" <?php the_sub_field('volunteer_image');?> alt="Card image cap">
+                                <img class="card-img-top fotoinsideVOL" src="<?php the_sub_field('vol_image');?>" alt="test" alt="Card image cap">
                                 <div class="card-body d-flex flex-column">
-                                    <h4 class="card-title"><?php the_sub_field('volunteer_name');?></h4>
+                                    <h4 class="card-title"><?php the_sub_field('vol_name');?></h4>
                                     <small>
-                                        <p><?php the_sub_field('volunteer_description');?></p>
+                                        <p><?php the_sub_field('vol_description');?></p>
                                     </small>
                                     <div class="volsocials row d=flex justify-content-between mx-2">
                                         <div class="volbuttonsright">
@@ -26,8 +26,8 @@
                                             <span class="ml-2"><a href=""><i class="fa fa-twitter"></i></a></span>
                                         </div>
                                         <div class="volbuttonsleft">
-                                            <span class="mr-2"><a href=""><i class="fa fa-envelope" ><?php the_sub_field('volunteer_email');?></i></a></span>
-                                            <span class=""><a href=""><i class="fa fa-phone" ><?php the_sub_field('volunteer_phone');?></i></a></span>
+                                            <span class="mr-2"><a href=""><i class="fa fa-envelope" ><?php the_sub_field('vol_mail');?></i></a></span>
+                                            <span class=""><a href=""><i class="fa fa-phone" ><?php the_sub_field('vol_phone');?></i></a></span>
                                         </div>
                                     </div>
                                 </div>
