e14dcdc51f565e2083bf6183bcbbcd76ab35cfc4
r2on11 <61480893+r2on11@users.noreply.github.com>
2020-06-25 14:25:43 +0200

    minor changes

--- modules/question-component.php
+++ modules/question-component.php
@@ -3,13 +3,13 @@
             <div class="questionFoto col-lg-6"></div>
             <div class="queskolright col-lg-6 d-flex justify-content-center align-items-center">
                 <div class=" quesrightcont d-flex flex-column " >
-                    <div class="questiontitulli"><h1>Got any questions?</br> See more below</h1></div>
+                    <div class="questiontitulli hideMe"><h1>Got any questions?</br> See more below</h1></div>
                     <div class="questionsbut">
                             <?php
                             if( have_rows('quescomp_parts') ):
                                 while ( have_rows('quescomp_parts') ) : the_row(); ?>
                                 
-                                    <div class="quesAll">
+                                    <div class="quesAll hideMe">
                                         <div class="menu">
                                             <div class="menu-item quesdiff toggler"><?php the_sub_field('quescomp_button');?></div>
                                             <div class="collapsible-wrapper collapsed">
