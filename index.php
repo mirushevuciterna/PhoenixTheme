abbad27e10e242bcd79ededba348ab5c8356fda7
r2on11 <61480893+r2on11@users.noreply.github.com>
2020-06-25 09:15:01 +0200

    Final

--- index.php
+++ index.php
@@ -3,12 +3,11 @@
     get_template_part('parts/DifferenceComponent');
     get_template_part('modules/causescomponent');
     get_template_part('parts/donateComponent');
+    get_template_part('parts/activeEventsComponent');
     get_template_part('modules/volunteerscomponent');
-    get_template_part('modules/questioncomponent');
-    get_template_part('parts/activeEventsComponent'); 
+    get_template_part('modules/questioncomponent'); 
     get_template_part('parts/logosComponent');
     get_template_part('parts/TextOnImage');
-
 ?>
 
 <?php get_footer(); ?>
--- modules/volunteerscomponent.php
+++ modules/volunteerscomponent.php
@@ -10,27 +10,28 @@
 
             <div class="owl-caroVOL owl-carousel owl-theme">
                 <?php
-                if( have_rows('vol_set') ):
-                while ( have_rows('vol_set') ) : the_row(); ?>
+                if( have_rows('volunteer_settings') ):
+                while ( have_rows('volunteer_settings') ) : the_row(); ?>
                 <div class="item foto1VOL">
                     <div class="card p-3 cardthyrjeVOL">
                         <img class="card-img-top fotoinsideVOL"
-                            src="<?php the_sub_field('vol_image'); ?>""
+                            src="<?php the_sub_field('volunteer_image'); ?>""
                             alt="Card image cap">
-                        <div class="card-body d-flex flex-column">
-                            <h4 class="card-title"><?php the_sub_field('vol_name'); ?></h4>
-                            <small>
-                                <p><?php the_sub_field('vol_description'); ?></p>
+                            <div class="card-body d-flex flex-column">
+                            <h4 class="card-title"><?php the_sub_field('volunteer_name'); ?></h4>
+                            <h5><?php the_sub_field('volunteer_title'); ?></h5>
+                            <small id="voltest">
+                                <p><?php the_sub_field('volunteer_description'); ?></p>
                             </small>
                             <div class="volsocials row d=flex justify-content-between mx-2">
                                 <div class="volbuttonsright">
                                     <span class=""><a href=""><i class="fa fa-facebook fa-sm"></i></a></span>
-                                    <span class="ml-2"><a href=""><i class="fa fa-linkedin"></i></a></span>
-                                    <span class="ml-2"><a href=""><i class="fa fa-twitter"></i></a></span>
+                                    <span class="ml-1"><a href=""><i class="fa fa-linkedin"></i></a></span>
+                                    <span class="ml-1"><a href=""><i class="fa fa-twitter"></i></a></span>
                                 </div>
                                 <div class="volbuttonsleft">
-                                    <span class="expbuttons mr-2"><a href=""><i class="fa fa-envelope" ><span id="afterexp"><?php the_sub_field('vol_mail'); ?></span></i></a></span>
-                                    <span class="expbuttons mr-2"><a href=""><i class="fa fa-phone" ><span id="afterexp"><?php the_sub_field('vol_phone'); ?></span></i></a></span>
+                                    <span class="expbuttons mr-1"><a href=""><i class="fa fa-envelope" ><span id="afterexp"><?php the_sub_field('volunteer_email'); ?></span></i></a></span>
+                                    <span class="expbuttons"><a href=""><i class="fa fa-phone" ><span id="afterexp"><?php the_sub_field('volunteer_phone'); ?></span></i></a></span>
                                 </div>
                             </div>
                         </div>
