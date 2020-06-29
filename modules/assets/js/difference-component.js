c9ce4fd7628c945059554ecd5c5e70a232072e18
pashtriga <pashtrik.gashi1@gmail.com>
2020-06-29 15:24:48 +0200

    minor changes

--- modules/assets/js/difference-component.js
+++ modules/assets/js/difference-component.js
@@ -1,5 +1,5 @@
 //diffcomponent
-if (document.location.pathname == "/") {
+if (document.location.pathname == "/" || document.location.pathname == "/wordpress_site/" || document.location.pathname == "/testsite/wordpress/"  || document.location.pathname == "/StarlabsWebPage/") {
     
 var countDownDate = new Date("Jun 5, 2021 15:37:25").getTime();
 
