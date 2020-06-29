c1f6b45463e257ac7eaafdf3f6069588b96e41d4
pashtriga <pashtrik.gashi1@gmail.com>
2020-06-29 16:25:39 +0200

    changed path name on donation-video.js

--- modules/assets/js/donation-video.js
+++ modules/assets/js/donation-video.js
@@ -1,15 +1,15 @@
 /** Donation Video Modal */
-if (document.location.pathname == "/") {
-function playDonationVideo(videoId) {
-    let videoContent = document.getElementById("donation-video-" + videoId).innerHTML;
-    let modal = document.getElementById("videoModal");
-    document.getElementById('donationModalContent').innerHTML = videoContent;
-    modal.style.display = "block";
-}
+if (document.location.pathname == "/" || document.location.pathname == "/wordpress_site/" || document.location.pathname == "/testsite/wordpress/" || document.location.pathname == "/StarlabsWebPage/") {
+    function playDonationVideo(videoId) {
+        let videoContent = document.getElementById("donation-video-" + videoId).innerHTML;
+        let modal = document.getElementById("videoModal");
+        document.getElementById('donationModalContent').innerHTML = videoContent;
+        modal.style.display = "block";
+    }
 
-let closeBtn = document.getElementsByClassName("close")[0];
-closeBtn.addEventListener("click", function(event) {
-    document.getElementById("videoModal").style.display = "none";
-    document.getElementById('donationModalContent').innerHTML = '';
-});
+    let closeBtn = document.getElementsByClassName("close")[0];
+    closeBtn.addEventListener("click", function (event) {
+        document.getElementById("videoModal").style.display = "none";
+        document.getElementById('donationModalContent').innerHTML = '';
+    });
 }
