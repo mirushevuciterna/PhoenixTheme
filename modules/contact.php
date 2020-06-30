2cdd374372aad4c8292e89c73c0d19088aee4e87
pashtriga <pashtrik.gashi1@gmail.com>
2020-06-30 14:08:20 +0200

    working on contact form

--- modules/contact.php
+++ modules/contact.php
@@ -5,46 +5,26 @@ Template Name: Contact
 ?>
 <?php
 if(isset($_POST['submit'])) {
-	if(trim($_POST['contactName']) === '') {
-		$nameError = 'Please enter your name.';
-		$hasError = true;
-	} else {
-		$name = trim($_POST['contactName']);
-	}
-
-	if(trim($_POST['email']) === '')  {
-		$emailError = 'Please enter your email address.';
-		$hasError = true;
-	} else if (!preg_match("/^[[:alnum:]][a-z0-9_.-]*@[a-z0-9.-]+\.[a-z]{2,4}$/i", trim($_POST['email']))) {
-		$emailError = 'You entered an invalid email address.';
-		$hasError = true;
-	} else {
-		$email = trim($_POST['email']);
-	}
-
-	if(trim($_POST['comments']) === '') {
-		$commentError = 'Please enter a message.';
-		$hasError = true;
-	} else {
-		if(function_exists('stripslashes')) {
-			$comments = stripslashes(trim($_POST['comments']));
-		} else {
-			$comments = trim($_POST['comments']);
-		}
-	}
-
-	if(!isset($hasError)) {
-		$emailTo = get_option('tz_email');
-		if (!isset($emailTo) || ($emailTo == '') ){
-			$emailTo = get_option('admin_email');
-		}
-		$subject = '[PHP Snippets] From '.$name;
-		$body = "Name: $name \n\nEmail: $email \n\nComments: $comments";
-		$headers = 'From: '.$name.' <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $email;
-
-		wp_mail($emailTo, $subject, $body, $headers);
-		$emailSent = true;
-	}
+	// Check for empty fields
+if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+    http_response_code(500);
+    exit();
+  }
+  
+  $name = strip_tags(htmlspecialchars($_POST['name']));
+  $email = strip_tags(htmlspecialchars($_POST['email']));
+  $phone = strip_tags(htmlspecialchars($_POST['phone']));
+  $message = strip_tags(htmlspecialchars($_POST['message']));
+  
+  // Create the email and send the message
+  $to = "pashtrik.gashi1@gmail.com"; // Add your email address in between the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
+  $subject = "Website Contact Form:  $name";
+  $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
+  $header = "From: noreply@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+  $header .= "Reply-To: $email";	
+  
+  if(!mail($to, $subject, $body, $header))
+    http_response_code(500);
 
 } ?>
 
@@ -54,7 +34,6 @@ if(isset($_POST['submit'])) {
 
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
         <div <?php post_class() ?> id="post-<?php the_ID(); ?>">
-            <h1 class="entry-title"><?php the_title(); ?></h1>
             <div class="entry-content">
                 <?php if(isset($emailSent) && $emailSent == true) { ?>
                 <div class="thanks">
@@ -62,18 +41,62 @@ if(isset($_POST['submit'])) {
                 </div>
                 <?php } else { ?>
                 <?php the_content(); ?>
-                <?php //if(isset($hasError) || isset($captchaError)) { ?>
-                <!-- <p class="error">Sorry, an error occured.<p> -->
-                        <?php// } ?>
+                
+                <!-- Contact Section-->
+        <section class="page-section" id="contact">
+            <div class="container">
+                <!-- Contact Section Heading-->
+                <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0"><?php the_title(); ?></h2>
+                <!-- Icon Divider-->
+                <div class="divider-custom">
+                    <div class="divider-custom-line"></div>
+                    <div class="divider-custom-icon"><i class="fa fa-star"></i></div>
+                    <div class="divider-custom-line"></div>
+                </div>
+                <!-- Contact Section Form-->
+                <div class="row contact-form">
+                    <div class="col-lg-8 mx-auto">
+                        <form action="<?php the_permalink(); ?>" id="contactForm" name="sentMessage" method="post" novalidate="novalidate">
+                            <div class="control-group">
+                                <div class="form-group floating-label-form-group controls mb-0 pb-2">
+                                    <label>Name</label><input class="form-control" id="name" type="text" placeholder="Name" required="required" data-validation-required-message="Please enter your name." />
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                            </div>
+                            <div class="control-group">
+                                <div class="form-group floating-label-form-group controls mb-0 pb-2">
+                                    <label>Email Address</label><input class="form-control" id="email" type="email" placeholder="Email Address" required="required" data-validation-required-message="Please enter your email address." />
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                            </div>
+                            <div class="control-group">
+                                <div class="form-group floating-label-form-group controls mb-0 pb-2">
+                                    <label>Phone Number</label><input class="form-control" id="phone" type="tel" placeholder="Phone Number" required="required" data-validation-required-message="Please enter your phone number." />
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                            </div>
+                            <div class="control-group">
+                                <div class="form-group floating-label-form-group controls mb-0 pb-2">
+                                    <label>Message</label><textarea class="form-control" id="message" rows="5" placeholder="Message" required="required" data-validation-required-message="Please enter a message."></textarea>
+                                    <p class="help-block text-danger"></p>
+                                </div>
+                            </div>
+                            <br />
+                            <div id="success"></div>
+                            <div class="form-group form-group-butoni"><button class="btn btn-lg donateBTN btn-xl" id="sendMessageButton" type="submit">Send</button></div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
 
-
-                        <form action="<?php the_permalink(); ?>" id="contactForm" method="post" class="my-3">
+        <!-- <form action="<?php the_permalink(); ?>" id="contactForm" method="post" class="my-3">
                             <div class="form-group">
                                 <label for="contactName">Name:</label>
                                 <input type="text" name="contactName" id="contactName"
                                     value="<?php if(isset($_POST['contactName'])) echo $_POST['contactName'];?>"
                                     class="required requiredField form-control" />
-                                <?php if($nameError != '') { ?>
+                                    <?php if($nameError != '') { ?>
                                 <span class="error"><?=$nameError;?></span>
                                 <?php } ?>
                             </div>
@@ -101,7 +124,8 @@ if(isset($_POST['submit'])) {
 							<br>
                             <input type="submit" class="btn btn-primary" value="Send Mail" name="submit">
 							
-                        </form>
+                        </form> -->
+
                         <?php } ?>
             </div><!-- .entry-content -->
         </div><!-- .post -->
