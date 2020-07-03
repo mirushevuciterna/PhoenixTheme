5bf4153c3a4bf1d28a831e4c85f59cde1d9ef746
Donika Tomiqi <donikatomiqi@gmail.com>
2020-07-03 14:15:11 +0200

    position button

--- modules/donation-video.php
+++ modules/donation-video.php
@@ -6,9 +6,9 @@ $videoIndex = 1;
             <div class="donationVideoFeature">
                 <?php $image = get_sub_field('image_video');
                 if( $image ) { ?>
-                <div class="video-container" style="background-image:url('<?= $image ?>')">
+                <div class="video-container d-flex justify-content-center align-items-center" style="background-image:url('<?= $image ?>')">
                 <?php  } ?>
-                    <div onclick="playDonationVideo(<?php echo $videoIndex; ?>)" class="playDiv hideMe" id="myBtn1111">
+                    <div onclick="playDonationVideo(<?php echo $videoIndex; ?>)" class="playDiv hideMe">
                         <i class="fa fa-play"></i>
                         <div id="donation-video-<?php echo $videoIndex; ?>" style="display: none">
                             <?php the_sub_field('video');?>
@@ -33,7 +33,7 @@ $videoIndex = 1;
 
                 <?php $image = get_sub_field('image_video');
                 if( $image ) { ?>
-                <div class="video-container" style="background-image:url('<?= $image ?>')">
+                <div class="video-container d-flex justify-content-center align-items-center" style="background-image:url('<?= $image ?>')">
                 <?php  } ?>
                     <div onclick="playDonationVideo(<?php echo $videoIndex; ?>)" class="playDiv hideMe" id="myBtn1111">
                         <i class="fa fa-play"></i>
