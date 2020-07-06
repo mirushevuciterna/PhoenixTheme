1544ff6e378d3acd52bfea1b2bcbf5b131140ea7
r2on11 <61480893+r2on11@users.noreply.github.com>
2020-07-06 14:25:10 +0200

    minor changes

--- modules/donation-video.php
+++ modules/donation-video.php
@@ -23,9 +23,11 @@ $videoIndex = 1;
                     </div>
                 </div>
                 <div class="donationVideoText d-flex flex-column justify-content-center align-items-center p-5 hideMe">
-                    <h3 class="donation-title"><?php the_sub_field('titulli_video');?></h3>
-                    <p class="donation-paragraph"><?php the_sub_field('paragrafi_video');?></p>
-                    <button class="btn btn-lg donateBTN"><?php the_sub_field('butoni_video');?></button>
+                    <div class="donationcontent px-5">
+                        <h3 class="donation-title"><?php the_sub_field('titulli_video');?></h3>
+                        <p class="donation-paragraph"><?php the_sub_field('paragrafi_video');?></p>
+                        <button class="btn btn-lg donateBTN"><?php the_sub_field('butoni_video');?></button>
+                    </div>
                 </div>
             </div>
 
