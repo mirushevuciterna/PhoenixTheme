f2d8582561bde40ed133bc361c10af19abcf7c7a
Donika Tomiqi <donikatomiqi@gmail.com>
2020-07-08 15:08:11 +0200

    minor changes

--- custom_register.php
+++ custom_register.php
@@ -39,14 +39,13 @@ echo '<div class="col-12 register-error"><strong>ERROR:</strong> You are logged 
 <div class="col-md-5">
 
 <?php 
-$args = array(
-    'redirect' => home_url().'/register/', 
-    );
-
+$args = array()
+;
 
 wp_login_form($args); ?>
 
 <p class="text-center"><a class="mr-2" href="<?php echo wp_registration_url(); ?>">Register Now</a>
+<span clas="regspan mx-2">·</span><a class="ml-2" href="<?php echo wp_lostpassword_url( ); ?>" title="Lost Password">Lost Password?</a></p>
 
 </div>
 
@@ -69,7 +68,7 @@ $error = 1;
 $email = esc_sql($_REQUEST['email']);
 if ( !preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/", $email) ) { 
 
-$emailError = 'You entered an invalid email address.';
+echo '<div class="col-12 register-error">Please enter a valid email.</div>';
 $error = 1;
 }
 
@@ -82,12 +81,15 @@ $error = 1;
 
 if ( $error == 0 ) {
 
+// $random_password = wp_generate_password( 12, false ); 
 $status = wp_create_user( $username, $password, $email ); 
 
 if ( is_wp_error($status) ) {
 
-echo '<div class="col-12 register-error">This user already exists. Please try another one.</div>'; 
+echo '<div class="col-12 register-error">This user already exists.</div>'; 
 } else {
+wp_redirect('/');
+exit;    
 
 $from = get_option('admin_email'); 
 $headers = 'From: '.$from . "\r\n"; 
@@ -96,7 +98,8 @@ $message = "Registration successful.\nYour login details\nUsername: $username\nP
 
 // Email password and other details to the user
 wp_mail( $email, $subject, $message, $headers ); 
-echo "Please check your email for login details. "; 
+
+echo "Please check your email for login details."; 
 
 $error = 2; // We will check for this variable before showing the sign up form. 
 }
@@ -113,11 +116,7 @@ if ( $error != 2 ) { ?>
         <form action="" method="post">
             <h1>Create Account</h1>
             <input type="text" placeholder="Username" name="username" value="<?php if( ! empty($username) ) echo $username; ?>">
-            
             <input type="email" placeholder="Email" name="email" value="<?php if( ! empty($email) ) echo $email; ?>">
-            <?php if($emailError != '') { ?>
-                                    <span class="error"><?=$emailError;?></span>
-                                <?php } ?>
             <input type="password" placeholder="Password" name="password" value="<?php if( ! empty($password) ) echo $password; ?>">
             <button class="registerbutton" name="submit">Sign Up</button>
         </form>
@@ -127,8 +126,8 @@ if ( $error != 2 ) { ?>
         <form method="post" name="loginform" id="loginform" action="<?php echo site_url( '/wp-login.php' ); ?>">
             <h1 class="registertitle">Sign in</h1>
  
-            <input  id="user_login" type="text" placeholder="Username or email" size="20" name="log">
-            <input id="user_pass" type="password" name="pwd" placeholder="Password">
+            <input  id="user_login" type="text" size="20" name="log">
+            <input id="user_pass" type="password" name="pwd">
             <button id="wp-submit" type="submit" value="Login" name="wp-submit" class="registerbutton" >Sign In</button>
         </form>
     </div>
@@ -162,9 +161,9 @@ if ( $error != 2 ) { ?>
 <?php }
 
 } else { ?>
-<div class="m-5 p-5">
+
 <p>You are logged in. Click <a href="<?php bloginfo('wpurl'); ?>">here to go home</a></p>
-</div>
+
 <?php } ?>
 
 </div>
