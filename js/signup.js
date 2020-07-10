d5f7982c37e932db20aad7eff2c99495625ca220
r2on11 <61480893+r2on11@users.noreply.github.com>
2020-07-10 12:15:02 +0200

    login fix

--- js/signup.js
+++ js/signup.js
@@ -88,39 +88,42 @@ function validate(event) {
 
 
 
-// LOGIN VALIDATION
-function validateLogin(event) {
 
-	let valid = true;
-	let text, error;
-	let x = document.getElementById("user_login").value;
-	let y = document.getElementById("user_pass").value;
+jQuery(document).ready(function($) {
+	$(".disable").prop('disabled',true); 
+    // Show the login dialog box on click
+    $('a#show_login').on('click', function(e){
+        $('body').prepend('<div class="login_overlay"></div>');
+        $('form#login').fadeIn(500);
+        $('div.login_overlay, form#login a.close').on('click', function(){
+            $('div.login_overlay').remove();
+            $('form#login').hide();
+        });
+        e.preventDefault();
+    });
+
+    // Perform AJAX login on form submit
+    $('form#login').on('submit', function(e){
+        $('form#login p.status').show().text(ajax_login_object.loadingmessage);
+        $.ajax({
+            type: 'POST',
+            dataType: 'json',
+            url: ajax_login_object.ajaxurl,
+            data: { 
+                'action': 'ajaxlogin', //calls wp_ajax_nopriv_ajaxlogin
+                'username': $('form#login #username').val(), 
+                'password': $('form#login #password').val(), 
+                'security': $('form#login #security').val() },
+            success: function(data){
+                $('form#login p.status').text(data.message);
+                if (data.loggedin == true){
+					document.location.href = ajax_login_object.redirecturl;
+					
+                }
+            }
+        });
+        e.preventDefault();
+	});
 	
-	if (x == null || x == "") {
-		text = "This field can't be blank!";
-		document.getElementById("demo").innerHTML = text;
-		valid = false;
-	} else {
-		document.getElementById("demo").innerHTML = "";
-		return true;
-	}
-	if (y.length < 6) {
-		error = "Password must contain at least 6 characters!"
-		document.getElementById("demo1").innerHTML = error;
-		valid = false;
-	} else {
-		document.getElementById("demo1").innerHTML = "";
-		return true;
-	}
 
-	if (!validateLogin) {
-	event.preventDefault();
-	let elements = document.getElementsByClassName('error-login');
-		for (let i = 0; i < elements.length; i++) {
-			elements[i].style.color = 'red';
-		}
-	}
-	return true;
-}
-
-
+});
