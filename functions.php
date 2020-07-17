c0726e0e55767cd9aebeded08aeab1072c048b2f
Donika Tomiqi <donikatomiqi@gmail.com>
2020-07-17 14:46:21 +0200

    bug fixes

--- functions.php
+++ functions.php
@@ -5,13 +5,11 @@ function register_navwalker(){
 add_action( 'after_setup_theme', 'register_navwalker' );
 
 function phoenix_script_enqueue(){
-   
-    wp_enqueue_style( 'bootstrap', '//stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css' );
+    wp_enqueue_style('bootstrap', get_stylesheet_directory_uri().'/css/bootstrap.min.css');
     wp_enqueue_style('customstyle', get_stylesheet_directory_uri().'/css/main.css');
     wp_enqueue_style('style', get_stylesheet_directory_uri().'/modules/assets/css/portfolio.css');
     wp_enqueue_style('owlcarouselmincss', get_stylesheet_directory_uri().'/css/owl.carousel.min.css');
     wp_enqueue_style('owlthemedefaultmincss', get_stylesheet_directory_uri().'/css/owl.theme.default.min.css');
-    wp_enqueue_script( 'jquery');
     wp_enqueue_script('boortstrapjs', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '1.0.0', true);
     wp_enqueue_style( 'pratafont', '//fonts.googleapis.com/css2?family=Prata&display=swap' );
     wp_enqueue_style( 'Montserrat', '//fonts.googleapis.com/css2?family=Montserrat:wght@800&display=swap' );
