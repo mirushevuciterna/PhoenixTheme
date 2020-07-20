d6ed683af05035aef7cbf4ec74598350b95d4122
Donika Tomiqi <donikatomiqi@gmail.com>
2020-07-20 18:12:40 +0200

    style changes as search icon

--- searchform.php
+++ searchform.php
@@ -2,7 +2,7 @@
     <div class="input-group">
       <input type="search" class="form-control" value="<?= get_search_query() ?>" placeholder="Search" name="s" title="Search">
       <div class="input-group-append">
-        <button class="btn btn-light" type="submit" name="submit">
+        <button class="btn bg-white border-0" type="submit" name="submit">
           <i class="fa fa-search"></i>
         </button>
       </div>
