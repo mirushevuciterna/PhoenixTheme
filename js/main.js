536463e75584e51225da9b65e59b2dd7e4eeebda
Donika Tomiqi <donikatomiqi@gmail.com>
2020-07-21 14:53:44 +0200

    added script for alt tags

--- js/main.js
+++ js/main.js
@@ -1,19 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-if ($(window).width() > 1000) {
-    $('.dropdown-toggle').click(function() {
-        window.location = $(this).attr('href'); 
-    });
-}
-$('.nav-link').removeAttr('title');
+    if ($(window).width() > 1000) {
+        $('.dropdown-toggle').click(function() {
+            window.location = $(this).attr('href'); 
+        });
+    }
+    $('.nav-link').removeAttr('title');
 
-// let images = document.querySelectorAll('img');
-// images.forEach(el => {   
-//     const alt = el.getAttribute('alt');
-//     const src = el.getAttribute('src').split("/").pop();
-//     if (!alt){
-//         el.setAttribute('alt', src);
-//     }  
-// });
+    let images = document.querySelectorAll('img');
+    images.forEach(el => {   
+        const alt = el.getAttribute('alt');
+        const src = el.getAttribute('data-src').split("/").pop();
+        const image_name = src.split('.').shift();
+        if (!alt){
+            el.setAttribute('alt', image_name);
+        }  
+    });
 
 });
