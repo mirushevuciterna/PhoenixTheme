c842486d067e5d370e416db4b4b42333fcba80f4
Donika Tomiqi <donikatomiqi@gmail.com>
2020-07-22 13:52:53 +0200

    social filter

--- functions.php
+++ functions.php
@@ -665,20 +665,32 @@ add_filter( 'rank_math/sitemap/posts_to_exclude', function( $posts_to_exclude ){
  * @param JsonLD $unsigned JsonLD instance.
  */
 
-add_filter( 'rank_math/json_ld', function( $data, $jsonld ) {
-    if(is_home() || is_single()) {
-        $twitter = get_the_author_meta( 'twitter', $post->post_author );
-        $facebook = get_the_author_meta( 'facebook', $post->post_author );
-        $data['BlogPosting'] = [
-            "@context" => "http://schema.org/",
-            "@type" => "BlogPosting",
-            "author" => [
-                "@type" => "Person",
-                "name" => get_the_author_meta('display_name', $author_id),
-                "sameAs" => ['https://twitter.com/' . $twitter, $facebook]
-            ]
-        ];
-        return $data;
-    }
-    return [];
-}, 10, 2);
+// add_filter( 'rank_math/json_ld', function( $data, $jsonld ) {
+//     if(is_home() || is_single()) {
+//         $twitter = get_the_author_meta( 'twitter', $post->post_author );
+//         $facebook = get_the_author_meta( 'facebook', $post->post_author );
+//         $data['BlogPosting'] = [
+//             "@context" => "http://schema.org/",
+//             "@type" => "BlogPosting",
+//             "author" => [
+//                 "@type" => "Person",
+//                 "name" => get_the_author_meta('display_name', $author_id),
+//                 "sameAs" => ['https://twitter.com/' . $twitter, $facebook]
+//             ]
+//         ];
+//         return $data;
+//     }
+//     return [];
+// }, 10, 2);
+
+add_filter( 'rank_math/snippet/rich_snippet_article_entity', function( $entity ) {
+    $twitter = get_the_author_meta( 'twitter', $post->post_author );
+    $facebook = get_the_author_meta( 'facebook', $post->post_author );
+    $entity['author'] = [
+        '@type' => 'Person',
+        'name'  => get_the_author_meta('display_name', $author_id),
+        'sameAs' => ['https://twitter.com/' . $twitter, $facebook]
+    ];
+
+    return $entity;
+});
