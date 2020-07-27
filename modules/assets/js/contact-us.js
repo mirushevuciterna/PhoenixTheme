c2ac2b27d2adaf2b4b04e928cd6caa47f01db81d
Donika Tomiqi <donikatomiqi@gmail.com>
2020-07-27 14:53:59 +0200

    fixed mixed content error

--- modules/assets/js/contact-us.js
+++ modules/assets/js/contact-us.js
@@ -93,7 +93,7 @@ function initialize() {
     });
 
 }
-// google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, 'load', initialize);
 
 ////////////
 $('.location-btn').hover(function() {
