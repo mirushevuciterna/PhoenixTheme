618ac6024e9de6a18fe1a50bd4d4cc412ddc1f7e
r2on11 <61480893+r2on11@users.noreply.github.com>
2020-07-28 10:28:10 +0200

    portfolio

--- archive.php
+++ archive.php
@@ -1,28 +1,40 @@
 <?php get_header(); ?>
 
-<div class="container-fluid">
+
     <?php 
 if( have_posts() ): while ( have_posts() ): the_post(); ?>
+<div class="archive-row row mb-5 p-3 d-flex justify-content-center">
+   
+        <div class="list-group-item list-group-item-action flex-column align-items-start">
 
-    <div class="list-group mb-5 py-3 col-12">
-        <a href="<?php echo the_permalink()?>"
-            class="list-group-item list-group-item-action flex-column align-items-start">
-            <div class="d-block d-md-flex w-100 justify-content-between ">
-                <small class="times-ago"><?php echo meks_time_ago() ?></small>
-                <h5 class="mb-1 order-last order-md-first"><?php the_title() ?></h5>        
-            </div>
-            <div class="col-12 col-md-6 pl-0">
-                <p class="mb-1"><?php the_excerpt() ?></p>
-                <?php //the_post_thumbnail() ?>
-                <img src="<?php echo get_the_post_thumbnail_url (); ?>" class="pl-0 col-12">
-            </div>
-        </a>
-    </div>
+                <div class="d-block d-md-flex w-100 justify-content-between my-2">
+                    <small class="times-ago"><?php echo meks_time_ago() ?></small>
+                    <h2 class="mb-1 order-last order-md-first"><?php the_title() ?></h2>        
+                </div>
+
+                <div class="row">
+                    <div class="col-md-6 pl-0">
+                        <?php //the_post_thumbnail() ?>
+                        <img src="<?php echo get_the_post_thumbnail_url (); ?>" class="col-12">
+                    </div>
+                    <div class="col-md-6 px-5 list-group d-flex flex-column justify-content-center">
+                        <div class="hideMe archive-content">
+                            <h4>Basic Info</h4>
+                            <p class="mb-1"><?php the_content() ?></p>
+                            <a class="post-link" href="<?php echo the_permalink(); ?>">Read more</a>
+
+                        </div>
+                        
+                    </div>
+                </div>
+        </div>
+    
+    
+</div>
 
     <?php endwhile;
 endif;
 ?>
-<?php post_pagination(); ?>
-</div>
+<div class="text-center"><?php post_pagination(); ?></div>
 
 <?php get_footer(); ?>
